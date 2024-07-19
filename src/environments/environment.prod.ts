d1a5d50a6c3522c86067036eea3a8821bfe65cfc
Nicolás Folli <nikofolli@gmail.com>
2024-07-19 01:55:55 -0300

    [FRONT] feat: nuevo proyecto

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    // ConexionWebApiProxy: 'https://nightops.azurewebsites.net/api/', // en un futuro disponibles desde internet
+    //ConexionWebApiProxy: 'http://localhost:25603/api/', // solo localmente
+    // ConexionWebApiProxy: 'https://localhost:443/api/', // solo franco
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+export const environment = {
+    // firebase: {
+    //   projectId: 'nightops-storage',
+    //   appId: '1:925758926651:web:24aabd8c3f0d4c5683d11e',
+    //   storageBucket: 'nightops-storage.appspot.com',
+    //   locationId: 'southamerica-east1',
+    //   apiKey: 'AIzaSyDyGE_wKy7omslx0EEDMJXGYAquinLX_TY',
+    //   authDomain: 'nightops-storage.firebaseapp.com',
+    //   messagingSenderId: '925758926651',
+    //   measurementId: 'G-9MV08NF458',
+    // },
+    production: false,
+    // ConexionWebApiProxy: 'https://nightops.azurewebsites.net/api/', // en un futuro disponibles desde internet
+    ConexionWebApiProxy: 'http://127.0.0.1:8000/', // solo localmente
+    // ConexionWebApiProxy: 'https://localhost:443/api/', // solo franco
+  };
