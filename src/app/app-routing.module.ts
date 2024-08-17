48045526abe078322e3d56a5c548b2a5e8e2427d
Nicolás Folli <nikofolli@gmail.com>
2024-08-17 20:52:34 -0300

    feat: componente home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './modules/components/home/home.component';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    { path: 'inicio', 
+      component: HomeComponent,
+    },
+    // { path: 'forbidden', loadChildren: () => import('./modules/public/forbidden/forbidden.module').then(m => m.ForbiddenModule) },
+    // { path: 'systemerror', loadChildren: () => import('./modules/public/system-error/system-error.module').then(m => m.SystemErrordModule) },
+    // { path: 'notfound', loadChildren: () => import('./modules/public/not-found/not-found.module').then(m => m.NotFoundModule) },
+    // { path: '**', redirectTo: '/notfound' },
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
