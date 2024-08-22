a6ecf8fd091defcc566d40595ad622b7ea758639
Nicolás Folli <nikofolli@gmail.com>
2024-08-22 12:10:27 -0300

    fix: intento arreglo carousel owl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppLayoutModule } from './layout/app.layout.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppLayoutModule } from './layout/app.layout.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AppLayoutModule,
   ],
--- src/app/modules/components/home/home.module.ts
+++ src/app/modules/components/home/home.module.ts
@@ -4,6 +4,9 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home.component';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 
 @NgModule({
@@ -12,6 +15,8 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
         FormsModule,
         ReactiveFormsModule,
         CarouselModule,
+        RouterModule,
+        BrowserAnimationsModule
 
     ],
     declarations: [
